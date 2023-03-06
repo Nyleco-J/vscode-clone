f858747a0defa9e8baae227498d649513435bb7f
Nyleco-J <anuran.nyleco24@gmail.com>
2023-03-06 15:36:30 +0800

    Vs Update

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ toggleMenu.addEventListener("click", () => {
   toggleMenu.classList.toggle("open");
   headerBot.classList.toggle("open");
 });
+// var slider = tns({
+//   container: '.slider__wrapper',
+//   items: 1,
+//   mouseDrag:true,
+//   controlsText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+//   responsive:{
+//       0:{
+//           items: 3,
+//       },
+//       481:{
+//           items: 1,
+//       }
+//   }
+// });
